1bc3d42607baac6ce14b41a707310d7b19e4c4b9
PiusKupfer <PiusKupfer@0-209-R5>
2021-09-24 09:12:14 +0200

    Initial commit

---
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -0,0 +1,12 @@
+
+public class TestVertex {
+	public static void main(String[]args){
+	Vertex v1 = new Vertex(17,4);
+	Vertex v2 = new Vertex(42,0);
+	Vertex v3 = new Vertex(0,0);
+	
+	System.out.println(v1);
+	System.out.println(v2);
+	System.out.println(v3);
+	}
+}
