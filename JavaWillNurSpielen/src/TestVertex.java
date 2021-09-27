5415556becc1d85a3c7af446f94cc9581c1a17de
AdminUser <AdminUser@HOME-PC.speedport.ip>
2021-09-27 21:12:57 +0200

    initial commit

--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -5,8 +5,22 @@ public class TestVertex {
 	Vertex v2 = new Vertex(42,0);
 	Vertex v3 = new Vertex(0,0);
 	
+
+	
+	
 	System.out.println(v1);
 	System.out.println(v2);
 	System.out.println(v3);
+	
+	int a=15;
+	int b=25;
+	
+	System.out.println(a+(b+"5"));
+	System.out.println(a+(5+"b"));
+	
+	
+	
 	}
+
+
 }
