6085a4d1659c9c34453176caf0116b60720edbe5
PiusKupfer <PiusKupfer@0-209-R5>
2021-10-01 09:11:10 +0200

    Initial Commtit

--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -1,22 +1,51 @@
 
 public class TestVertex {
 	public static void main(String[]args){
+		
+		String s1 = "hallo";
+		String s2 = "Hallo" .toLowerCase();
+		
+		
+		
+		
+		
 	Vertex v1 = new Vertex(17,4);
-	Vertex v2 = new Vertex(42,0);
+	Vertex v2 = new Vertex(42,23);
 	Vertex v3 = new Vertex(0,0);
+	Vertex v4 = new Vertex(42,23);
 	
-
 	
 	
+	System.out.println(v2 .toString());
+	System.out.println(v2);
+	System.out.println(v1.length());
+	System.out.println(v2.length());
+	System.out.println(v3.length());
+	System.out.println(v1.skalarMult(0.8));
+	System.out.println(v1);
+	System.out.println(v1.add(v2));
 	System.out.println(v1);
 	System.out.println(v2);
-	System.out.println(v3);
+	System.out.println(v1.equals(v2));
+	System.out.println(v2.equals(v4));
+	System.out.println(v4.equals(v2));
 	
-	int a=15;
-	int b=25;
 	
-	System.out.println(a+(b+"5"));
-	System.out.println(a+(5+"b"));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	System.out.println(s1 .contentEquals(s2));
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s1 == s2);
 	
 	
 	
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -3,16 +3,78 @@ public class Vertex {
 	public double x;
 	public double y;
 
-public Vertex(double x, double y) {	
-	this.x = x;
-	this.y = y;
-	
-	
+	public Vertex(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-public String toString() {
-		
-	return "x - "+x+", y - "+y;
-	
-}
 
+	public String toString() {
+		return "x = " + x + ", y= " + y;
+	}
+
+	public double length() {
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public Vertex skalarMult(double s) {
+		return new Vertex(x * s, y * s);
+	}
+	
+	public void skalarMultMod (double s) {
+		x=s*x;
+		y=s*y;
+	}
+
+	public Vertex add (Vertex v2) {
+		return new Vertex (x+v2.x,y+v2.y);
+	}
+	
+	public Vertex sub(Vertex v2) {
+		return new Vertex (x-v2.x,y-v2.y);
+	}
+	
+	public double distance(Vertex v2) {
+		
+		double xx= v2.x-this.x;
+		double yy= v2.y-this.y;
+		
+		return  Math.sqrt(xx*xx + yy*yy);
+	}
+	
+	public void normalized(Vertex v2) {
+		double factor =this.length();
+		x = x / factor;
+		y = y / factor;
+	}
+	
+	public void addMod(Vertex v2) {
+		x=x+v2.x;
+		y=y+v2.y;
+	}
+	
+	public void setX(double x) {
+		this.x=x;
+	}
+	
+	public void setY(double y) {
+		this.y=y;
+	}
+	
+	public double getX(double x) {
+		return x;
+	}
+	
+	public double getY(double y) {
+		return y;
+	}
+	
+	public boolean equals(Object thatObject) {
+		if(thatObject instanceof Vertex) {
+			Vertex that = (Vertex)thatObject;
+		return this.x==that.x && this.y ==that.y;		
+				}
+		
+	return false;
+		
+	}
 }
